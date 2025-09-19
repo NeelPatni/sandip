4a696db1ed388c124c504dab1a528aaa5226fa6b
Neel Patni <neelpatni14@gmail.com>
2025-09-19 13:42:48 +0530

    Updated backend: server.js, contact and career form

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(cors({
-  origin: ["https://sandipnanavati.com", "http://127.0.0.1:5500", "http://localhost:5500"],
+  origin: ["https://sandipnanavati.com", "https://www.sandipnanavati.com", "http://127.0.0.1:5500", "http://localhost:5500"],
   methods: ["GET", "POST"],
   allowedHeaders: ["Content-Type"],
 }));
